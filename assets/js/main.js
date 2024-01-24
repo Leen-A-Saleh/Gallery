b7db6e9864a7e64d6e87c3f4e2351fcae2b8df49
Leen-A-Saleh <https://github.com/Leen-A-Saleh>
2024-01-24 18:26:51 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,189 @@
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+    /* ====== Preloader ======*/
+    const preloader = document.querySelector('#preloader');
+    if (preloader) {
+      window.addEventListener('load', () => {
+        setTimeout(() => {
+          preloader.classList.add('loaded');
+        }, 1000);
+        setTimeout(() => {
+          preloader.remove();
+        }, 2000);
+      });
+    }
+     /* ======  line preloading ====== */
+     function aos_init() {
+      AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+      });
+    }
+    window.addEventListener('load', () => {
+      aos_init();
+    });
+  });
+    /* ======= Mobile nav toggle ====== */
+    const mobileNavShow = document.querySelector('.mobile-nav-show');
+    const mobileNavHide = document.querySelector('.mobile-nav-hide');
+  
+    document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+      el.addEventListener('click', function(event) {
+        event.preventDefault();
+        mobileNavToogle();
+      })
+    });
+    function mobileNavToogle() {
+      document.querySelector('body').classList.toggle('mobile-nav-active');
+      mobileNavShow.classList.toggle('d-none');
+      mobileNavHide.classList.toggle('d-none');
+    }
+    /* ====== Hide mobile nav on same-page/hash links ====== */
+
+    document.querySelectorAll('#navbar a').forEach(navbarlink => {
+  
+      if (!navbarlink.hash) return;
+  
+      let section = document.querySelector(navbarlink.hash);
+      if (!section) return;
+  
+      navbarlink.addEventListener('click', () => {
+        if (document.querySelector('.mobile-nav-active')) {
+          mobileNavToogle();
+        }
+      });
+    });
+    /* ======= Toggle mobile nav dropdowns ====== */
+    const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+    navDropdowns.forEach(el => {
+      el.addEventListener('click', function(event) {
+        if (document.querySelector('.mobile-nav-active')) {
+          event.preventDefault();
+          this.classList.toggle('active');
+          this.nextElementSibling.classList.toggle('dropdown-active');
+  
+          let dropDownIndicator = this.querySelector('.dropdown-indicator');
+          dropDownIndicator.classList.toggle('bi-chevron-up');
+          dropDownIndicator.classList.toggle('bi-chevron-down');
+        }
+      })
+    });
+    /* ====== Scroll top button ======= */
+    let calcScrollValue = () => {
+      let scrollProgress = document.getElementById("progress");
+      let progressValue = document.getElementById("progress-value");
+      let pos = document.documentElement.scrollTop;
+      let calcHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      let scrollValue = Math.round((pos * 100) / calcHeight);
+      if (pos > 50) {
+        scrollProgress.style.display = "grid";
+      } else {
+        scrollProgress.style.display = "none";
+      }
+      scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+      });
+      scrollProgress.style.background = `conic-gradient(#b8889f ${scrollValue}%, #000 ${scrollValue}%)`;
+    };
+    window.onscroll = calcScrollValue;
+    window.onload = calcScrollValue;
+    /* ======= Initiate glightbox ====== */
+    const glightbox = GLightbox({
+      selector: '.glightbox'
+    });
+    // active link in navbar
+    const page = window.location.pathname;
+    const pageName = page.split("/").pop();
+    if(pageName === "index.html"){
+      document.querySelector(".index").classList.add("navActive");
+    }
+    if(pageName === "about.html"){
+      document.querySelector(".about").classList.add("navActive");
+    }
+    if(pageName === "photographers.html"){
+      document.querySelector(".photographers").classList.add("navActive");
+    }
+    if(pageName === "services.html" || pageName === "more.html"){
+      document.querySelector(".services").classList.add("navActive");
+    }
+    if(pageName === "booking.html"){
+      document.querySelector(".booking").classList.add("navActive");
+    }
+    // about==>counter-up
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 250;
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+/* ======  booking  (5)slide show in desktop view ====== */
+    new Swiper('.slides-3', {
+      speed: 600,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        },
+  
+        1200: {
+          slidesPerView: 3,
+        }
+      }
+    });
+ /* ======  booking  (15)slide show in desktop view ====== */
+ new Swiper('.slides-1', {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 40
+    },
+
+    1200: {
+      slidesPerView: 1,
+    }
+  }
+});
